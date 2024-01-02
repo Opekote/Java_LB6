8acaef67269b925b1dc8e398c6758fe79d7a2d14
Opekote <pochtastud@gmail.com>
2024-01-02 17:49:25 +0200

    v0.3

--- src/main/java/org/university/Cinema.java
+++ src/main/java/org/university/Cinema.java
@@ -16,8 +16,8 @@ public class Cinema {
     // Method to book seats
     public boolean bookSeats(int hallNumber, int row, int[] seats) {
         for (int seat : seats) {
-            if (cinemaSeats[hallNumber-1][row-1][seat-1] == 0) {
-                cinemaSeats[hallNumber-1][row-1][seat-1] = 1; // 1 represents a booked seat
+            if (cinemaSeats[hallNumber][row][seat] == 0) {
+                cinemaSeats[hallNumber][row][seat] = 1; // 1 represents a booked seat
                 System.out.println("Seat booked: Hall " + hallNumber + ", Row " + row + ", Seat " + seat);
             } else {
                 System.out.println("Some of requested seats are already booked.Please choose another seats!");
@@ -32,8 +32,8 @@ public class Cinema {
     // Method to cancel booking
     public boolean cancelBooking(int hallNumber, int row, int[] seats) {
         for (int seat : seats) {
-            if (cinemaSeats[hallNumber-1][row-1][seat-1] == 1) {
-                cinemaSeats[hallNumber-1][row-1][seat-1] = 0; // 0 represents an available seat
+            if (cinemaSeats[hallNumber][row][seat] == 1) {
+                cinemaSeats[hallNumber][row][seat] = 0; // 0 represents an available seat
                 System.out.println("Booking canceled: Hall " + hallNumber + ", Row " + row + ", Seat " + seat);
             } else {
                 System.out.println("No booking found for seat " + seat + " .Ensure the correctness of data for");
@@ -45,10 +45,10 @@ public class Cinema {
 
     // Method to check availability of consecutive seats
     public boolean checkAvailability(int hallNumber, int numSeats) {
-        for (int i = 0; i < cinemaSeats[hallNumber-1].length; i++) {
+        for (int i = 0; i < cinemaSeats[hallNumber].length; i++) {
             int consecutiveSeats = 0;
-            for (int j = 0; j < cinemaSeats[hallNumber-1][i].length; j++) {
-                if (cinemaSeats[hallNumber-1][i][j] == 0) {
+            for (int j = 0; j < cinemaSeats[hallNumber][i].length; j++) {
+                if (cinemaSeats[hallNumber][i][j] == 0) {
                     consecutiveSeats++;
                     if (consecutiveSeats == numSeats) {
                         System.out.println("Consecutive seats available in Hall " + hallNumber + ", Row " + i);
@@ -68,20 +68,20 @@ public class Cinema {
 
         System.out.println("     1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20");
 
-        for (int i = 0; i < cinemaSeats[hallNumber-1].length; i++) {
+        for (int i = 0; i < cinemaSeats[hallNumber].length; i++) {
             // Showing number of a row
             System.out.printf("%2d | ", i+1);
 
             // Showing Seats in a row
-            for (int j = 0; j < cinemaSeats[hallNumber-1][i].length; j++) {
+            for (int j = 0; j < cinemaSeats[hallNumber][i].length; j++) {
                 if (j >= 9){
-                    System.out.printf("%2d ", cinemaSeats[hallNumber-1][i][j]);
+                    System.out.printf("%2d ", cinemaSeats[hallNumber][i][j]);
                 }
                 else {
-                    System.out.print(cinemaSeats[hallNumber-1][i][j] + " ");
+                    System.out.print(cinemaSeats[hallNumber][i][j] + " ");
                 }
             }
-            System.out.printf("| %2d", i+1);
+            System.out.printf("| %2d", i);
 
             System.out.println();
 
@@ -93,13 +93,13 @@ public class Cinema {
     public List<int[]> findBestAvailable(int hallNumber, int numSeats) {
         List<int[]> bestSeats = new ArrayList<>();
 
-        for (int i = 0; i < cinemaSeats[hallNumber-1].length; i++) {
-            for (int j = 0; j <= cinemaSeats[hallNumber-1][i].length - numSeats; j++) {
+        for (int i = 0; i < cinemaSeats[hallNumber].length; i++) {
+            for (int j = 0; j <= cinemaSeats[hallNumber][i].length - numSeats; j++) {
                 boolean seatsAvailable = true;
 
                 // Checking availability of seats
                 for (int k = j; k < j + numSeats; k++) {
-                    if (cinemaSeats[hallNumber-1][i][k] != 0) {
+                    if (cinemaSeats[hallNumber][i][k] != 0) {
                         seatsAvailable = false;
                         break;
                     }
@@ -111,7 +111,10 @@ public class Cinema {
                     for (int k = 0; k < numSeats; k++) {
                         seatRange[k] = j + k;
                     }
-                    bestSeats.add(new int[]{i, seatRange[0]});
+                    int[] seatInfo = new int[numSeats + 1]; //Creating a new array to save there info about row and seat numbers
+                    seatInfo[0] = i; // Row number
+                    System.arraycopy(seatRange, 0, seatInfo, 1, numSeats); // Copying seatRange to seatInfo
+                    bestSeats.add(seatInfo);
                 }
             }
         }
@@ -124,15 +127,15 @@ public class Cinema {
 
         if (!bestSeats.isEmpty()) {
             // Booking first best place
-            int[] seatInfo = bestSeats.get(0);
-            int row = seatInfo[0];
-            int seat = seatInfo[1];
-            int[] seats = Arrays.copyOfRange(seatInfo,1,seatInfo.length-1);
-            bookSeats(hallNumber, seatInfo[0], seats);
+            int[] seatInfo = bestSeats.get(0); //Using first result as best available
+            int row = seatInfo[0]; //getting info about row. (We are using first element in our array as a row number)
+            int[] seats = Arrays.copyOfRange(seatInfo,1,seatInfo.length);
+            System.out.println(Arrays.toString(seats));
+            bookSeats(hallNumber, row, seats);
 
 
 
-            System.out.println("Successfully booked " + numSeats + " seats in Hall " + hallNumber + " row " + row + ".");
+            System.out.println("Successfully booked " + numSeats + " seats in Hall " + hallNumber + " row " + row + " seats " + Arrays.toString(seats));
             return true;
         } else {
             System.out.println("Sorry, there are no available seats for " + numSeats + " in Hall " + hallNumber + ".");
--- src/test/java/CinemaTest.java
+++ src/test/java/CinemaTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.university.Cinema;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CinemaTest {
@@ -16,23 +16,23 @@ public class CinemaTest {
 
     @Test
     void bookSeatsTest(){
-        Assertions.assertTrue(cinema.bookSeats(5,10,new int[]{5,6,7}));
+        Assertions.assertTrue(cinema.bookSeats(4,9,new int[]{5,6,7}));
     }
     @Test
     void bookInvalidSeatsTest(){
-        cinema.bookSeats(5,10,new int[]{5,6,7});
-        Assertions.assertFalse(cinema.bookSeats(5,10,new int[]{5,6,7}));
+        cinema.bookSeats(4,9,new int[]{5,6,7});
+        Assertions.assertFalse(cinema.bookSeats(4,9,new int[]{5,6,7}));
     }
 
     @Test
     void cancelBookingTest(){
-        cinema.bookSeats(5,10,new int[]{5,6,7});
-        Assertions.assertTrue(cinema.cancelBooking(5,10,new int[]{5,6,7}));
+        cinema.bookSeats(4,9,new int[]{5,6,7});
+        Assertions.assertTrue(cinema.cancelBooking(4,9,new int[]{5,6,7}));
     }
 
     @Test
     void invalidCancelBookingTest(){
-        Assertions.assertFalse(cinema.cancelBooking(5,10,new int[]{5,6,7}));
+        Assertions.assertFalse(cinema.cancelBooking(4,9,new int[]{5,6,7}));
     }
 
     @Test
@@ -46,21 +46,19 @@ public class CinemaTest {
         cinema.printSeatingArrangement(1);
     }
 
-//    @Test
-//    void findBestAvailableTest(){
-//
-//
-//        List<int[]> given = cinema.findBestAvailable(1,3);
-//
-//        List<int[]> expected = new ArrayList<>();
-//        expected.add(new int[]{0,0,1,2});
-//        Assertions.assertArrayEquals(expected.get(0),given.get(0));
-//    }
+    @Test
+    void findBestAvailableTest(){
+
+
+        List<int[]> bestSeats = cinema.findBestAvailable(0, 4);
+        System.out.println("Best seats are " + Arrays.toString(bestSeats.get(0)));
+        Assertions.assertArrayEquals(new int[]{0, 0, 1, 2, 3}, bestSeats.get(0));
+    }
 
 
     @Test
     void autoBookTest(){
-        Assertions.assertTrue(cinema.autoBook(1,3));
+        Assertions.assertTrue(cinema.autoBook(1,5));
     }
 
 }
